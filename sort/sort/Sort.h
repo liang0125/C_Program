fdaf8e1463a26f1e81a050edf5491c3c5087a1fd
liang0125 <952787172@qq.com>
2018-11-09 17:43:00 +0800

    sort

--- sort/sort/Sort.h
+++ sort/sort/Sort.h
@@ -110,10 +110,104 @@ void Print(int array[],int size)
 		printf("%d ", array[i]);
 	}
 }
+void AdjustDown(int array[], int size, int root)
+{
+	int left = 2 * root + 1;
+	int right = 2 * root + 2;
+	if (left >= size)
+	{
+		return;
+	}
+	int max = left;
+	if (right<size&&array[right]>array[left])
+	{
+		max = right;
+	}
+	if (array[root] >= array[max])
+	{
+		return;
+	}
+	Swap(array + root, array + max);
+	AdjustDown(array, size, max);
+}
+void CreateHeap(int array[], int size)
+{
+	//从最后一个非叶子结点到0
+	//不断的进行向下调整
+	for (int i = size / 2 - 1; i >= 0; i--)
+	{
+		AdjustDown(array, size, i);
+	}
+}
+void HeapSort(int array[], int size)
+{
+	CreateHeap(array, size);
+	for (int i = 0; i < size; i++)
+	{
+		Swap(array, array + (size - i - 1));
+		AdjustDown(array,size-i-1,0);
+	}
+}
+void BubbleSort(int array[], int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		int flag = 1;
+		for (int j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				Swap(array + j, array + j + 1);
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+		{
+			break;
+		}
+	}
+}
+int Partition(int array[], int left, int right)
+{
+	int begin = left;
+	int end = right;
+	while (begin < end)
+	{
+		while (begin < end&&array[begin] <= array[right])
+		{
+			begin++;
+		}
+		while (begin < end&&array[end] >= array[right])
+		{
+			end--;
+		}
+		Swap(array + begin, array + end);
+	}
+	Swap(array + begin, array + right);
+	return begin;
+}
+void _QuickSort(int array[],int left,int right)
+{
+	int div = Partition(array, left, right);
+	if (left == right)
+	{
+		return;
+	}
+	if (left > right)
+	{
+		return;
+	}
+	_QuickSort(array, left, div - 1);
+	_QuickSort(array, div + 1, right);
+}
+void QuickSort(int array[], int size)
+{
+	_QuickSort(array, 0, size - 1);
+}
 void test()
 {
 	int array[] = { 3,5,1,4,7,2,6,0,9,8,8 };
 	int size = sizeof(array) / sizeof(int);
-	SelectSort1(array, size);
+	QuickSort(array, size);
 	Print(array,size);
 }
